7e35559f94c13e6ec9bb2341ebe23a3897de42fe
向吕 <lv.xiang@hualongdata.com>
2019-07-30 20:21:58 +0800

    修改json时间设置

--- src/main/java/org/spring/springboot/app/base/Constants.java
+++ src/main/java/org/spring/springboot/app/base/Constants.java
@@ -2,14 +2,11 @@ package org.spring.springboot.app.base;
 
 /**
  * 常用参数
- *
- * @author ry
- * @date 2018/9/25
  */
 public class Constants {
     public static final String TOKEN_PARAM_NAME = "token";
     public static final String TOKEN_SESSION_NAME = "getToken";
 
-    public static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd hh:mm:ss";
+    public static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
 
 }
